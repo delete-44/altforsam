96516c07cd11450e51a3f04d60b98ae357a4f409
delete-44 <delete44+tech@protonmail.com>
2025-11-18 15:23:34 -0800

    Added preview

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { generateAltText } from "./src/generate-alt.js";
 
+const INERT_TEXT = "📋 Copy alt to clipboard";
+const COPIED_TEXT = "✅ Copied!";
+
 /**
  * Event handler for the copy button;
  * * responds to click;
@@ -11,16 +14,48 @@ import { generateAltText } from "./src/generate-alt.js";
  * @returns {*}
  */
 const handleCopyButtonClick = async function () {
+  const copyButton = document.getElementById("copy-button");
   const resultsInput = document.getElementById("results-input");
 
-  if (!resultsInput) {
+  if (!resultsInput || !copyButton) {
     return;
   }
 
   const altText = generateAltText(resultsInput.value);
 
   await navigator.clipboard.writeText(altText);
+  copyButton.innerText = COPIED_TEXT;
+
+  setTimeout(() => {
+    copyButton.innerText = INERT_TEXT;
+  }, 1250);
 };
+
+/**
+ * Event handler for the results input;
+ * * responds to change;
+ * * generates the alt text
+ * * loads it into the preview window
+ *
+ * @async
+ * @returns {*}
+ */
+const handleResultsInputChange = function () {
+  const resultsInput = document.getElementById("results-input");
+  const previewInput = document.getElementById("preview-input");
+
+  if (!resultsInput || !previewInput) {
+    return;
+  }
+
+  const altText = generateAltText(resultsInput.value);
+
+  previewInput.innerHTML = altText;
+};
+
+document
+  .getElementById("results-input")
+  .addEventListener("input", handleResultsInputChange);
 
 document
   .getElementById("copy-button")
